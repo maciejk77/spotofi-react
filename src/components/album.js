6d9c278f61d262fbc9608bef475f5d488de3d846
Maciej Kurek <maciejk77@gmail.com>
2018-07-20 23:29:52 +0100

    layout with tables

--- src/components/album.js
+++ src/components/album.js
@@ -9,13 +9,51 @@ const Album = (props) => {
  
   return ( 
     //if(!props) { return null } 
-    <div className="card">
-      <div>Genre: {data.category.attributes.term}</div>
-      <div>Artist: {data['im:artist'].label}</div>
-      <div>IMAGE: <img src={data['im:image'][1].label} /></div>
-      <div>Album Name: {data['im:name'].label}</div>
+    <div className="album">
+      
+      <table>
+                
+        <thead className="thead">
+          <tr className="tr">
+            <td className="td">{data['im:name'].label}</td>
+          </tr>
+        </thead>
+
+
+      </table>
+      
+      <table>
+       
+      
+        {/* <thead className="thead">
+          <tr className="tr">
+            <td className="td">{data['im:name'].label}</td>
+          </tr>
+        </thead> */}
+
+        <thead className="thead">
+        <tr className="tr">
+          <th className="th"><img src={data['im:image'][1].label} /></th>
+          <td className="td">{data.category.attributes.term}</td>
+          <td className="td">{data['im:artist'].label}</td>
+        </tr>
+      </thead>
+
+      <thead className="thead">
+        <tr className="tr">
+          <td className="td">{data['im:releaseDate'].attributes.label}</td>
+          <td className="td">{data['im:price'].attributes.amount} {data['im:price'].attributes.currency}</td>
+        </tr>
+      </thead>
+
+    </table>
+
+      {/* <div>Genre: {data.category.attributes.term}</div>
+      <div>Artist: {data['im:artist'].label}</div> */}
+      {/* <div>IMAGE: <img src={data['im:image'][1].label} /></div> */}
+      {/* <div>Album Name: {data['im:name'].label}</div>
       <div>Price: {data['im:price'].attributes.amount} {data['im:price'].attributes.currency}</div>
-      <div>Release Date: {data['im:releaseDate'].attributes.label}</div>
+      <div>Release Date: {data['im:releaseDate'].attributes.label}</div> */}
       <a href={data.link.attributes.href} target="_blank">LINK TO APPLE STORE</a>
 
       {/* <div>MUSIC: {data['im:contentType'].attributes.term}</div> */}
