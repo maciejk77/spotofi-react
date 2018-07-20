cb505c4f0ec51c5492e3babffa82fec792b5e0b0
Maciej Kurek <maciejk77@gmail.com>
2018-07-20 20:20:46 +0100

    one item of API data rendered

--- src/components/album.js
+++ src/components/album.js
@@ -3,15 +3,24 @@ import React from 'react';
 const Album = (props) => {
   //debugger
   console.log(props);
-  return (
+
+  const data = props.data.feed;
+  return (  
     <div>
-      <div>SOUNDTRACK: {props.data.feed.entry[0].category.attributes.term}</div>
-      <div>SOUNDTRACK: {props.data.feed.entry[0].category.attributes.term}</div>
-      <div>SOUNDTRACK: {props.data.feed.entry[0].category.attributes.term}</div>
-      <div>SOUNDTRACK: {props.data.feed.entry[0].category.attributes.term}</div>
-      <div>SOUNDTRACK: {props.data.feed.entry[0].category.attributes.term}</div>
-      <div>SOUNDTRACK: {props.data.feed.entry[0].category.attributes.term}</div>
-      <div>SOUNDTRACK: {props.data.feed.entry[0].category.attributes.term}</div>
+      <div>SOUNDTRACK: {data.entry[0].category.attributes.term}</div>
+      <div>ARTIST: {data.entry[0]['im:artist'].label}</div>
+      <div>MUSIC: {data.entry[0]['im:contentType'].attributes.term}</div>
+      <div>ALBUM: {data.entry[0]['im:contentType']['im:contentType'].attributes.term}</div>
+      <div>IMAGE-SML: <img src={data.entry[0]['im:image'][0].label} /></div>
+      <div>IMAGE-MDM: <img src={data.entry[0]['im:image'][1].label} /></div>
+      <div>IMAGE-BIG: <img src={data.entry[0]['im:image'][2].label} /></div>
+      <div>ITEM-COUNT?: {data.entry[0]['im:itemCount'].label}</div>
+      <div>IM:NAME?: {data.entry[0]['im:name'].label}</div>
+      <div>IM:PRICE?: {data.entry[0]['im:price'].attributes.amount} {data.entry[0]['im:price'].attributes.currency}</div>
+      <div>RELEASE DATE: {data.entry[0]['im:releaseDate'].attributes.label}</div>
+      <div>LINK: {data.entry[0].link.attributes.href}</div>
+      <div>TITLE: {data.entry[0].title.label}</div>
+      <div>-------------------------------------------------------------------------------------</div>
     </div>
   )
 }
