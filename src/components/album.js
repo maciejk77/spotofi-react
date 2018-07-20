34d72019e0db5ab3da6f3f6d03e043aa1e37cecb
Maciej Kurek <maciejk77@gmail.com>
2018-07-20 22:37:12 +0100

    a bit of styling

--- src/components/album.js
+++ src/components/album.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/album.css';
 
 const Album = (props) => {
   //debugger
@@ -8,25 +9,23 @@ const Album = (props) => {
  
   return ( 
     //if(!props) { return null } 
-    <div>
-      <div>SOUNDTRACK: {data.category.attributes.term}</div>
-      <div>ARTIST: {data['im:artist'].label}</div>
-      <div>MUSIC: {data['im:contentType'].attributes.term}</div>
-      <div>ALBUM: {data['im:contentType']['im:contentType'].attributes.term}</div>
-      <div>IMAGE-SML: <img src={data['im:image'][0].label} /></div>
-      <div>IMAGE-MDM: <img src={data['im:image'][1].label} /></div>
-      <div>IMAGE-BIG: <img src={data['im:image'][2].label} /></div>
-      <div>ITEM-COUNT?: {data['im:itemCount'].label}</div>
-      <div>IM:NAME?: {data['im:name'].label}</div>
-      <div>IM:PRICE?: {data['im:price'].attributes.amount} {data['im:price'].attributes.currency}</div>
-      <div>RELEASE DATE: {data['im:releaseDate'].attributes.label}</div>
-      <div>LINK: {data.link.attributes.href}</div>
-      <div>TITLE: {data.title.label}</div>
-      <div>-------------------------------------------------------------------------------------</div>
+    <div className="card">
+      <div>Genre: {data.category.attributes.term}</div>
+      <div>Artist: {data['im:artist'].label}</div>
+      <div>IMAGE: <img src={data['im:image'][1].label} /></div>
+      <div>Album Name: {data['im:name'].label}</div>
+      <div>Price: {data['im:price'].attributes.amount} {data['im:price'].attributes.currency}</div>
+      <div>Release Date: {data['im:releaseDate'].attributes.label}</div>
+      <a href={data.link.attributes.href} target="_blank">LINK TO APPLE STORE</a>
+
+      {/* <div>MUSIC: {data['im:contentType'].attributes.term}</div> */}
+      {/* <div>ALBUM: {data['im:contentType']['im:contentType'].attributes.term}</div> */}
+      {/* <div>IMAGE-SML: <img src={data['im:image'][0].label} /></div> */}
+      {/* <div>IMAGE-BIG: <img src={data['im:image'][2].label} /></div> */}
+      {/* <div>ITEM-COUNT?: {data['im:itemCount'].label}</div> */}
+      {/* <div>TITLE: {data.title.label}</div> */}
     </div>
   )
 }
 
 export default Album;
-
-// props.data.feed.entry[0].title.label
