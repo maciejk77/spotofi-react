56ee9f77249c61f6cea98963891c002b06e79234
Maciej Kurek <maciejk77@gmail.com>
2018-07-20 20:57:58 +0100

    more on accessing API

--- src/components/album.js
+++ src/components/album.js
@@ -1,28 +1,31 @@
 import React from 'react';
 
 const Album = (props) => {
-  //debugger
+  debugger
   console.log(props);
 
-  const data = props.data.feed;
+  const { data } = props;
+ 
   return (  
     <div>
-      <div>SOUNDTRACK: {data.entry[0].category.attributes.term}</div>
-      <div>ARTIST: {data.entry[0]['im:artist'].label}</div>
-      <div>MUSIC: {data.entry[0]['im:contentType'].attributes.term}</div>
-      <div>ALBUM: {data.entry[0]['im:contentType']['im:contentType'].attributes.term}</div>
-      <div>IMAGE-SML: <img src={data.entry[0]['im:image'][0].label} /></div>
-      <div>IMAGE-MDM: <img src={data.entry[0]['im:image'][1].label} /></div>
-      <div>IMAGE-BIG: <img src={data.entry[0]['im:image'][2].label} /></div>
-      <div>ITEM-COUNT?: {data.entry[0]['im:itemCount'].label}</div>
-      <div>IM:NAME?: {data.entry[0]['im:name'].label}</div>
-      <div>IM:PRICE?: {data.entry[0]['im:price'].attributes.amount} {data.entry[0]['im:price'].attributes.currency}</div>
-      <div>RELEASE DATE: {data.entry[0]['im:releaseDate'].attributes.label}</div>
-      <div>LINK: {data.entry[0].link.attributes.href}</div>
-      <div>TITLE: {data.entry[0].title.label}</div>
+      <div>SOUNDTRACK: {data.category.attributes.term}</div>
+      <div>ARTIST: {data['im:artist'].label}</div>
+      <div>MUSIC: {data['im:contentType'].attributes.term}</div>
+      <div>ALBUM: {data['im:contentType']['im:contentType'].attributes.term}</div>
+      <div>IMAGE-SML: <img src={data['im:image'][0].label} /></div>
+      <div>IMAGE-MDM: <img src={data['im:image'][1].label} /></div>
+      <div>IMAGE-BIG: <img src={data['im:image'][2].label} /></div>
+      <div>ITEM-COUNT?: {data['im:itemCount'].label}</div>
+      <div>IM:NAME?: {data['im:name'].label}</div>
+      <div>IM:PRICE?: {data['im:price'].attributes.amount} {data['im:price'].attributes.currency}</div>
+      <div>RELEASE DATE: {data['im:releaseDate'].attributes.label}</div>
+      <div>LINK: {data.link.attributes.href}</div>
+      <div>TITLE: {data.title.label}</div>
       <div>-------------------------------------------------------------------------------------</div>
     </div>
   )
 }
 
 export default Album;
+
+// props.data.feed.entry[0].title.label
--- src/components/album_list.js
+++ src/components/album_list.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Album from './album.js';
 
 const AlbumList = (props) => {
+  if(!props) { return null }
   return (
     <div>
-      <Album data={props.data}/>
+      <Album data={props.data.feed.entry[0]}/>
     </div>
   )
 }
